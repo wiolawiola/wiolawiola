49b7564efeaa9786053bf1cee96eadc238e2b04f
Wiola <you@example.com>
2019-12-03 10:10:41 +0100

    first vexample

---
+++ Project1/Project1/HelloWorldTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Project1
+{
+    public class HelloWorldTest
+    {
+        [Fact]
+        public void CanSayHello()
+        {
+            Assert.True(true);
+        }        
+    }
+}
