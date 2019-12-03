e3c7627941b93950153c32971e1e13735c730628
Wiola <you@example.com>
2019-12-03 13:14:01 +0100

    PublishComment

--- Project1/Project1/GoogleTest.cs
+++ Project1/Project1/GoogleTest.cs
@@ -25,8 +25,35 @@ namespace Project1
             queryBox.Submit();
 
             var result = browser.FindElement(By.Id("wob_tm"));
-                Assert.Equal("3", result.Text);
-            
+            Assert.Equal("3", result.Text);
+
+        }
+
+        [Fact]
+
+        public void Can_add_new_comment()
+        {
+            browser.Navigate().GoToUrl("http://automatyzacja.benedykt.net/");
+
+            var queryBox = browser.FindElement(By.CssSelector(".entry-title"));
+            var link = queryBox.FindElement(By.TagName("a"));
+            link.Click();
+            var queryBoxComment = browser.FindElement(By.Id("comment"));
+            queryBoxComment.Click();
+            queryBoxComment.SendKeys("Komentarz2");
+            var queryBoxAuthot = browser.FindElement(By.Id("author"));
+            queryBoxAuthot.SendKeys("Autor");
+            var queryBoxEmail = browser.FindElement(By.Id("email"));
+            queryBoxEmail.SendKeys("igarrazuxo-6741@yopmail.com");
+            var PublishComment = browser.FindElement(By.Id("submit"));
+            PublishComment.Click();
+            var contentComment = browser.FindElement(By.CssSelector(".comment-content"));
+            var y = contentComment.FindElement(By.TagName("p"));
+
+
+            Assert.Equal("Komentarz2", y.Text );
+
+
         }
 
         public void Dispose()
